898b67a28a2bc69a775b3f20e001fb5ab372f01e
Andre Tischenko <39458631+Fuegos@users.noreply.github.com>
2022-06-18 01:02:12 +0400

    TASK-21 Adapting navigation panel for laptop and mobile

---
+++ client/src/components/MenuBar.jsx
@@ -0,0 +1,131 @@
+import * as React from 'react';
+import { Box, ListItemIcon, Menu, MenuItem, Divider, IconButton, Tooltip } from '@mui/material';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import LoginIcon from '@mui/icons-material/Login'
+import HowToRegIcon from '@mui/icons-material/HowToReg'
+import LogoutIcon from '@mui/icons-material/Logout'
+import { FormattedMessage } from 'react-intl'
+import { signOutUser } from '../authUser/redux/authUserSlice'
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings'
+import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark'
+
+
+export default function MenuBar() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const isAuth = useSelector(state => state.authUser.isAuth)
+    
+
+    const [anchorEl, setAnchorEl] = React.useState(null)
+    const open = Boolean(anchorEl)
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget)
+    }
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
+
+    const signIn =  !isAuth &&
+        <MenuItem onClick={() => navigate('auth/sign/in')}>
+            <ListItemIcon>
+                <LoginIcon />
+            </ListItemIcon>
+            
+            <FormattedMessage
+                    id="nav.sign.in"
+                    defaultMessage="Sign In"
+            />
+        </MenuItem>
+        
+
+    const signUp = !isAuth &&
+        <MenuItem onClick={() => navigate('auth/sign/up')}>
+            <ListItemIcon>
+                <HowToRegIcon />
+            </ListItemIcon>
+            
+            <FormattedMessage
+                id="nav.sign.up"
+                defaultMessage="Sign Up"
+            />
+        </MenuItem>
+        
+
+    const signOut = isAuth &&
+        <MenuItem
+            onClick={() => {
+                dispatch(signOutUser())
+                navigate('auth/sign/in')
+            }}
+        >
+            <ListItemIcon>
+                <LogoutIcon />
+            </ListItemIcon>
+
+            <FormattedMessage
+                id="nav.sign.out"
+                defaultMessage="Sign Out"
+            />
+        </MenuItem>
+
+    const myCollections = isAuth && 
+        <MenuItem>
+            <ListItemIcon>
+                <CollectionsBookmarkIcon />
+            </ListItemIcon>
+
+            <FormattedMessage
+                id="nav.my.collections"
+                defaultMessage="My Collections"
+            />
+        </MenuItem>
+    
+    const adminPanel = isAuth &&
+        <MenuItem>
+            <ListItemIcon>
+                <AdminPanelSettingsIcon />
+            </ListItemIcon>
+
+            <FormattedMessage
+                id="nav.admin"
+                defaultMessage="Admin Panel"
+            />
+        </MenuItem>
+
+    return (
+        <React.Fragment>
+            <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                <Tooltip title="My options">
+                    <IconButton
+                        onClick={handleClick}
+                        size="large"
+                        aria-controls={open ? 'account-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                    >
+                        <MenuOpenIcon fontSize='large' />
+                    </IconButton>
+                </Tooltip>
+            </Box>
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                {signIn}
+                {signUp}
+                {myCollections}
+                {adminPanel}
+                {isAuth && <Divider /> }
+                {signOut}
+            </Menu>
+        </React.Fragment>
+    )
+}
--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,93 +1,30 @@
-import { AppBar, Box, Toolbar, Container, Button, Grid } from '@mui/material'
-import LoginIcon from '@mui/icons-material/Login'
-import HowToRegIcon from '@mui/icons-material/HowToReg'
-import LogoutIcon from '@mui/icons-material/Logout'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { signOutUser } from '../authUser/redux/authUserSlice'
+import { AppBar, Box, Toolbar, Container } from '@mui/material'
 import SelectorLand from '../lang/components/SelectorLang'
 import SelectorThemeMode from '../themeMode/components/SelectorThemeMode'
-import { FormattedMessage } from 'react-intl'
+import MenuBar from './MenuBar'
 
 
 export default function NavigationApp() {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+    
+    const selectorLang = 
+        <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
+            <SelectorLand />
+        </Box>
 
-    const isAuth = useSelector(state => state.authUser.isAuth)
-        
-    const toolBarButtons = () => {
-        if(!isAuth) {
-            return (
-                <Toolbar disableGutters>
-                    <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
-                        <Button
-                            sx={{ my: 1, color: 'white' }}
-                            startIcon={<LoginIcon />}
-                            onClick={() => navigate('auth/sign/in')}
-                            fullWidth
-                        >
-                            <FormattedMessage
-                                id="nav.sign.in"
-                                defaultMessage="Sign In"
-                            />
-                        </Button>
-                    </Box>
-                    <Box sx={{ flexGrow: 1, display: {  md: 'flex' } }}>
-                        <Button
-                            sx={{ my: 1, color: 'white' }}
-                            startIcon={<HowToRegIcon />}
-                            onClick={() => navigate('auth/sign/up')}
-                            fullWidth
-                        >
-                            <FormattedMessage
-                                id="nav.sign.up"
-                                defaultMessage="Sign Up"
-                            />
-                        </Button>
-                    </Box>
-                    <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
-                        <SelectorLand />
-                    </Box>
-                    <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
-                        <SelectorThemeMode />
-                    </Box>
-                </Toolbar>
-            )
-        } else {
-            return (
-                <Toolbar disableGutters>
-                    <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
-                        <Button
-                            sx={{ my: 1, color: 'white' }}
-                            startIcon={<LogoutIcon />}
-                            onClick={() => {
-                                dispatch(signOutUser())
-                                navigate('auth/sign/in')
-                            }}
-                            fullWidth
-                        >
-                            <FormattedMessage
-                                id="nav.sign.out"
-                                defaultMessage="Sign Out"
-                            />
-                        </Button>
-                    </Box>
-                    <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
-                        <SelectorLand />
-                    </Box>
-                    <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
-                        <SelectorThemeMode />
-                    </Box>
-                </Toolbar>
-            )
-        }
-    }
+    const selectorTheme = 
+        <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
+            <SelectorThemeMode />
+        </Box>
+
 
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
-                {toolBarButtons()}
+                <Toolbar disableGutters>
+                    {selectorTheme}
+                    {selectorLang}
+                    <MenuBar />
+                </Toolbar>
             </Container>
         </AppBar>
     )
